6bc79eab3976cd19ca68d6590425ab79fa79a07b
Manuel Herdez <herdez@gmail.com>
2024-08-25 17:01:19 -0600

    simple roulette

--- data.js
+++ data.js
@@ -2,8 +2,8 @@
 export const labels = [
     // {
     //   "id": 1,
-    //   "name": "Gamificación",
-    //   "color": "#bd7093"
+    //   "name": "😅 Tema libre",
+    //   "color": "black"
     // },
     {
       "id": 2,
@@ -30,29 +30,54 @@ export const labels = [
       "name": "📚 Caso",
       "color": "DarkRed"
     },
-    {
-      "id": 7,
-      "name": "🛠 Proyecto",
-      "color": "SteelBlue"
-    },
-    {
-      "id": 8,
-      "name": "🎮 Juego",
-      "color": "MediumSeaGreen"
-    },
-    {
-      "id": 9,
-      "name": "🤼‍♀️ Reto",
-      "color": "DarkGoldenrod"
-    },
-    {
-      "id": 10,
-      "name": "🎬 Acción",
-      "color": "PaleVioletRed	"
-    },
-    {
-      "id": 11,
-      "name": "📚 Caso",
-      "color": "DarkRed"
-    },
+    // {
+    //   "id": 7,
+    //   "name": "🛠 Proyecto",
+    //   "color": "SteelBlue"
+    // },
+    // {
+    //   "id": 8,
+    //   "name": "🎮 Juego",
+    //   "color": "MediumSeaGreen"
+    // },
+    // {
+    //   "id": 9,
+    //   "name": "🤼‍♀️ Reto",
+    //   "color": "DarkGoldenrod"
+    // },
+    // {
+    //   "id": 10,
+    //   "name": "🎬 Acción",
+    //   "color": "PaleVioletRed	"
+    // },
+    // {
+    //   "id": 11,
+    //   "name": "📚 Caso",
+    //   "color": "DarkRed"
+    // },
+    // {
+    //   "id": 12,
+    //   "name": "🛠 Proyecto",
+    //   "color": "SteelBlue"
+    // },
+    // {
+    //   "id": 13,
+    //   "name": "🎮 Juego",
+    //   "color": "MediumSeaGreen"
+    // },
+    // {
+    //   "id": 14,
+    //   "name": "🤼‍♀️ Reto",
+    //   "color": "DarkGoldenrod"
+    // },
+    // {
+    //   "id": 15,
+    //   "name": "🎬 Acción",
+    //   "color": "PaleVioletRed	"
+    // },
+    // {
+    //   "id": 16,
+    //   "name": "📚 Caso",
+    //   "color": "DarkRed"
+    // },
   ]
