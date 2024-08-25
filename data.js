f7261f396298c12c9facc536ea15c3dd4dc89247
Manuel Herdez <herdez@gmail.com>
2024-08-25 16:55:28 -0600

    doble value

--- data.js
+++ data.js
@@ -30,4 +30,29 @@ export const labels = [
       "name": "📚 Caso",
       "color": "DarkRed"
     },
+    {
+      "id": 7,
+      "name": "🛠 Proyecto",
+      "color": "SteelBlue"
+    },
+    {
+      "id": 8,
+      "name": "🎮 Juego",
+      "color": "MediumSeaGreen"
+    },
+    {
+      "id": 9,
+      "name": "🤼‍♀️ Reto",
+      "color": "DarkGoldenrod"
+    },
+    {
+      "id": 10,
+      "name": "🎬 Acción",
+      "color": "PaleVioletRed	"
+    },
+    {
+      "id": 11,
+      "name": "📚 Caso",
+      "color": "DarkRed"
+    },
   ]
